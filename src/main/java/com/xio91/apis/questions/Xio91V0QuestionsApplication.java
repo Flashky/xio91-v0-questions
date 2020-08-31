d4ed2fb5c2484584abbfc8f22e31dad3cfab8a12
Flashky <mevengar@gmail.com>
2020-08-31 17:12:55 +0200

    Initial project structure

---
+++ src/main/java/com/xio91/apis/questions/Xio91V0QuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.xio91.apis.questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xio91V0QuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Xio91V0QuestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xio91/apis/questions/Xio91V0QuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xio91.apis.questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Xio91V0QuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
