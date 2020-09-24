e9245a29a37dd33ade141f2444ab2f7f957190bd
Flashk <mevengar@gmail.com>
2020-09-24 21:14:16 +0200

    Added self link mapping to question assembler

--- src/main/java/com/xio91/apis/questions/services/mappers/QuestionModelAssembler.java
+++ src/main/java/com/xio91/apis/questions/services/mappers/QuestionModelAssembler.java
@@ -1,20 +1,39 @@
 package com.xio91.apis.questions.services.mappers;
 
 import org.mapstruct.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.EntityLinks;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 
 import com.xio91.apis.questions.controllers.QuestionsRestController;
 import com.xio91.apis.questions.controllers.model.Question;
 import com.xio91.apis.questions.repositories.entities.QuestionEntity;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @Mapper(componentModel = "spring")
 public abstract class QuestionModelAssembler extends RepresentationModelAssemblerSupport<QuestionEntity, Question>{
 
+	@Autowired
+	private EntityLinks entityLinks;
+	
 	public QuestionModelAssembler() {
 	    super(QuestionsRestController.class, Question.class);
 	  }
 	
-	public abstract Question toModel(QuestionEntity entity);
+	public Question toModel(QuestionEntity entity) {
+		
+		Question questionModel = map(entity);
+		
+		// Add links
+		
+		Link self = linkTo(methodOn(QuestionsRestController.class).getQuestion(entity.getId())).withSelfRel();
+		questionModel.add(self);
+		
+		return questionModel;
+	}
+	
+	public abstract Question map(QuestionEntity entity);
 	
 	public abstract QuestionEntity toEntity(Question model);
  
