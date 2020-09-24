6d87c3addd4c8672923b320147b5c62334eec3b3
Flashk <mevengar@gmail.com>
2020-09-24 08:38:15 +0200

    Fixed sorting
    Sorting was fix by createdDate, now it depends on the pageable sort parameters.
    
    Pending to do: set a default sort.

--- src/main/java/com/xio91/apis/questions/repositories/QuestionsMongoRepository.java
+++ src/main/java/com/xio91/apis/questions/repositories/QuestionsMongoRepository.java
@@ -1,27 +1,9 @@
 package com.xio91.apis.questions.repositories;
 
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.xio91.apis.questions.repositories.entities.QuestionEntity;
 
-//@RepositoryRestResource(collectionResourceRel = "questions", path = "questions")
 public interface QuestionsMongoRepository extends MongoRepository<QuestionEntity, String> {
-	
-	@Override
-	default Page<QuestionEntity> findAll(Pageable pageable) {
-	    return findAllByOrderByCreatedDateDesc(pageable);
-	}
 
-	
-	@RestResource(exported = false)
-	Page<QuestionEntity> findAllByOrderByCreatedDateDesc(Pageable pageable);
-	
-	@RestResource(path = "author", rel = "author")
-	List<QuestionEntity> findByAuthor(String author);
 }
